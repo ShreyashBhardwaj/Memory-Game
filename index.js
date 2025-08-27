a7b2134230b64d8b4a7b141871451d4da456354b
Shreyash Bhardwaj <shreyashbhardwaj2002@gmail.com>
2025-08-27 15:02:43 +0530

    Ok so I think the GameMode() is done lets go to userMode()

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
 const sounds = [
-  "sounds/blue.mp3",
-  "sounds/green.mp3",
-  "sounds/red.mp3",
-  "sounds/yellow.mp3",
-  "sounds/wrong.mp3",
+  "/sounds/blue.mp3",
+  "/sounds/green.mp3",
+  "/sounds/red.mp3",
+  "/sounds/yellow.mp3",
+  "/sounds/wrong.mp3",
 ];
 
-let gamePattern = [];
+const buttons = ["#green", "#red", "#yellow", "#blue"];
+
+let gamePattern = ["#blue", "#green", "#yellow", "#red"];
 let userSelection = [];
+
+// Click Listeners for the Buttons
 
 let button = document.querySelectorAll(".btn");
 
-document.addEventListener("keydown", function () {
+for (var i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", function () {
+    console.log("click");
+  });
+}
+
+// Game Start
+document.addEventListener("keypress", function () {
   gameStart();
+  console.log("Game Start");
 });
 
 function gameStart() {
-  let gameStart = true;
   let gameLevel = 0;
   let title = document.querySelector("#level-title");
 
-  for (var i = 0; i < button.length; i++) {
-    button[i].addEventListener("click", game);
-  }
+  gameMode();
+  //   playerMode();
 
-  /*  while (gameStart) {
-    title.innerHTML = "Level " + gameLevel;
+  //   for (gameLevel = 0; gameLevel < gamePattern.length; gameLevel++) {
+  //     title.innerHTML = "Level " + (gameLevel + 1);
+  //   }
+}
 
-    gameLevel++;
-  }*/
+function gameMode() {
+  var buttonClicked = document.querySelector(gamePattern[0]).getAttribute("id");
+  console.log(buttonClicked);
+  animateOnClick(buttonClicked);
+  playSound(buttonClicked);
 }
 
 // Function to Generate pattern
@@ -42,20 +57,19 @@ function check(gamePattern, userSelection) {}
 // Sound to be played
 
 function playSound(buttonClicked) {
+  console.log(buttonClicked);
   let soundToBePlayed;
   switch (buttonClicked) {
-    case "g":
+    case "green":
       soundToBePlayed = sounds[1];
       break;
-    case "b":
+    case "blue":
       soundToBePlayed = sounds[0];
-
       break;
-    case "y":
+    case "yellow":
       soundToBePlayed = sounds[3];
-
       break;
-    case "r":
+    case "red":
       soundToBePlayed = sounds[2];
 
       break;
@@ -72,4 +86,11 @@ function playSound(buttonClicked) {
 
 // Funciton For Animation
 
-function animateOnClick(buttonClicked) {}
+function animateOnClick(buttonClicked) {
+  console.log(buttonClicked);
+  let activeButton = document.getElementById(buttonClicked);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
